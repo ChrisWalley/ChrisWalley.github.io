22a4ab749e5208773ffc36fbb0310f9ce809ec92
ChrisWalley <Chriswalley6@gmail.com>
2020-01-14 14:29:06 +0200

    Changed download files

--- pass.js
+++ pass.js
@@ -1,10 +1,23 @@
-function isValid(myNode){
-var password = document.getElementById('password').value;
-if (password == "tempPass")
+function isValid(){
+  return(document.getElementById('password').value.s() + "asdgfjhgyue".s() + 2*"grywvdv".s() == 64858126)
+};
+
+function show(){
+if (isValid())
   {
     document.getElementById("password").style.display = "none";
     document.getElementById("btnPass").style.display = "none";
-    document.getElementById("ArgusEx").style.display = "block";
     document.getElementById("Argus").style.display = "block";
   }
-}
+};
+
+String.prototype.s = function() {
+  var u = 0, i, c;
+  if (this.length === 0) return u;
+  for (i = 0; i < this.length; i++) {
+    c   = this.charCodeAt(i);
+    u  = ((u << 5) - u) + c;
+    u |= 0;
+  }
+  return u;
+};
